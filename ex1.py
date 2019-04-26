b93935d6a2b05ea4db404278a63ae0fb36d4cf54
iefan <mybsppp@163.com>
2019-04-26 11:39:30 +0800

    创建第1个版本

--- ex1.py
+++ ex1.py
@@ -1,24 +1,29 @@
 # import PySimpleGUI as sg 
 import PySimpleGUI as sg 
 import turtle as tt 
+import tkinter.font
+from tkinter import filedialog
 
-layout = [[sg.Canvas(size=(500, 500), background_color='white', key='canvas'),],      
-                 [sg.Multiline(size=(50, 5), key='command')],      
-                 [sg.Button("运行"), sg.Button("退出")]]      
+layout = [[sg.Canvas(size=(500, 500), background_color='white', key='canvas'),sg.Multiline(size=(100//7, 20//7), key='history', font=('Helvetica', 12))],      
+                 [sg.InputText(focus=True,do_not_clear=False, size=(540//10, 20//7), key='command', font=('Helvetica', 16)),], 
+                 [sg.Button("运行", font=('Helvetica', 16)), sg.Button("退出", font=('Helvetica', 16)), sg.T(" "*92), sg.Button("执行文件", font=('Helvetica', 16))]]      
 
-window = sg.Window('Window Title').Layout(layout).Finalize()    
+window = sg.Window('小海龟绘图').Layout(layout).Finalize()    
+
+# charpixels = tkinter.font.Font().measure('A')
+window.FindElement('history').Update("命令历史:")
+window.FindElement('history').Update(disabled=True)  
 
 canvas = window.FindElement('canvas').TKCanvas
 t = tt.RawTurtle(canvas)
 t.pencolor("#ff0000") # Red
 
-# t.penup()   # Regarding one of the comments
 t.pendown() # Regarding one of the comments
 t.fd(100)
-t.pu()
 
 while True:
     event, values = window.Read()
+    print(values)
     if event is None or event == "退出":
         break
     if event is '运行':
@@ -26,8 +31,29 @@ while True:
         if tmpcmd != "":
             try:
                 eval("t."+tmpcmd)
+                tmphistory = values['history'] + tmpcmd
+                # historywindow.Update(tmphistory)
+                # print(tmphistory, '----')
+                window.FindElement('history').Update(disabled=False)  
+                window.FindElement('history').Update(tmphistory)
+                window.FindElement('history').Update(disabled=True)
             except:
+                print(tmpcmd)
                 pass
+    if event is "执行文件":
+        # filedialog.askopenfilename(initialdir ='.')
+        file_name = filedialog.askopenfilename(filetypes=(("小海龟角本文件","*.psy"),), initialdir ='.') # show the 'get file' dialog box
+        print(len(file_name), type(file_name))
+        strcmd = "from turtle import *\n"
+        if len(file_name)==0:
+            pass
+        else:
+            with open(file_name) as f:
+                strcmd += f.read()
+            print(strcmd)
+            exec(strcmd, {}, {'t':t})
+        # print(t, type(t))
+        # exec("print(t, type(t),'==')", {}, {"t":t})
 
         # print(values["command"], type(values))
         
