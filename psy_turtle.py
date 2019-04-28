54bebc0073cc58d1e5ba2035043ccd2894a48205
iefan <mybsppp@163.com>
2019-04-28 11:05:25 +0800

    将multiline替换为listbox

--- psy_turtle.py
+++ psy_turtle.py
@@ -5,89 +5,78 @@ import tkinter.font
 from tkinter import filedialog
 import tkinter
 
-layout = [[sg.Canvas(size=(500, 500), background_color='white', key='_canvas_'),sg.Multiline(size=(100//7, 20//7), key='_history_', font=('宋体', 12))],      
-                 [sg.InputText(change_submits=False, focus=True,do_not_clear=False, size=(510//10, 20//7), key='_command_', font=('宋体', 16)),], 
+
+# menuHistory = ["导出menu", ["导出",]]
+layout = [[sg.Canvas(size=(500, 500), background_color='white', key='_canvas_'),\
+            sg.Listbox(values=["命令历史："], size=(15, 25), key='_history_', font=('宋体', 12), bind_return_key=True,)],
+                 [sg.InputText(focus=True, size=(510//10, 20//7), key='_command_', font=('宋体', 16)),], 
                  [sg.Button("运行", font=('宋体', 16)), sg.Button("退出", font=('宋体', 16)), sg.T(" "*96), sg.Button("执行文件", font=('宋体', 16))]]      
 
-window = sg.Window('小海龟绘图').Layout(layout).Finalize()    
+window = sg.Window('小海龟绘图', return_keyboard_events=True).Layout(layout).Finalize()    
 
-historywindow = window.FindElement('_history_').TKText
-def doubleClickSelToCommand(event):
-    # actual_callback(event) # this will select the word
-    w = historywindow.selection_get()
-    
+#================= 绑定doubleclick =================================
+# historywindow = window.FindElement('_history_').TKText
+# def doubleClickSelToCommand(event):
+#     # actual_callback(event) # this will select the word
+#     w = historywindow.selection_get()
+#     # print(historywindow.get(1,2), 'get===========')
+#     window.FindElement('_command_').Update(w)
+#     # print(w)
+# historywindow.bind("<Double-Button-1>", lambda e: historywindow.after(2, doubleClickSelToCommand, e)) # wait 2 ms before running callback
+#=====================================================================
 
-    # print(historywindow.get(1,2), 'get===========')
-    window.FindElement('_command_').Update(w)
-    # print(w)
-historywindow.bind("<Double-Button-1>", lambda e: historywindow.after(2, doubleClickSelToCommand, e)) # wait 2 ms before running callback
-
-
-commandwindow = window.FindElement('_command_').TKEntry
-def enterKeyRun(event):
-    tmpcmd = commandwindow.get().strip()
-    if tmpcmd != "":
-        try:
-            tmpcmd = "t." + tmpcmd
-            eval(tmpcmd)
-            tmphistory = historywindow.get("1.0", "end")
-            tmphistory += tmpcmd[2:]
-            # print(window.FindElement('_history_').TKText.get("1.0", "end"))
-            window.FindElement('_history_').Update(disabled=False)  
-            window.FindElement('_history_').Update(tmphistory)
-            window.FindElement('_history_').Update(disabled=True)
-            # print(1)
-        except:
-            pass
-            # sg.MsgBox("不可识别的!")
-        commandwindow.delete(0, 'end')
-commandwindow.bind('<Return>',enterKeyRun)
+#================= 绑定return =================================
+# commandwindow = window.FindElement('_command_').TKEntry
+# def enterKeyRun(event):
+#     tmpcmd = commandwindow.get().strip()
+#     if tmpcmd != "":
+#         try:
+#             tmpcmd = "t." + tmpcmd
+#             eval(tmpcmd)
+#             tmphistory = historywindow.get("1.0", "end")
+#             tmphistory += tmpcmd[2:]
+#             # print(window.FindElement('_history_').TKText.get("1.0", "end"))
+#             window.FindElement('_history_').Update(disabled=False)  
+#             window.FindElement('_history_').Update(tmphistory)
+#             window.FindElement('_history_').Update(disabled=True)
+#             # print(1)
+#         except:
+#             pass
+#             # sg.MsgBox("不可识别的!")
+#         commandwindow.delete(0, 'end')
+# commandwindow.bind('<Return>',enterKeyRun)
+#=====================================================================
 
 # charpixels = tkinter.font.Font().measure('A')
-window.FindElement('_history_').Update("命令历史:")
-window.FindElement('_history_').Update(disabled=True)
+# window.FindElement('_history_').Update("命令历史:")
+# window.FindElement('_history_').Update(disabled=True)
 
 
 canvas = window.FindElement('_canvas_').TKCanvas
 t = tt.RawTurtle(canvas)
-t.pencolor("#0000ff") # Red
-
-# t.pendown() # Regarding one of the comments
-# t.fd(100)
 
 while True:
     event, values = window.Read()
-    print(event, '===', values)
     if event is None or event == "退出":
         break
     
-    # if event=="_command_" :
-    #     print(values['_command_'])
-    #     if values['_command_']=="" : #回车
-    #         print('return')
-    #     else:
-    #         print(values['_command_'])
-    #         # print(window.FindElement('_command_').Get())
-    #         tmpcmd = window.FindElement('_command_').Get().strip()+values['_command_']
-    #         window.FindElement('_command_').Update(tmpcmd)
-
-    if event is '运行':
+    if event == "_history_":
+        if "命令历史" not in values['_history_'][0]:
+            window.FindElement('_command_').Update(values['_history_'][0])
+        
+    if event == "\r" or event is "运行":
         tmpcmd = values["_command_"].strip()
         if tmpcmd != "":
             try:
                 eval("t."+tmpcmd)
-                tmphistory = values['_history_'] + tmpcmd
-                # historywindow.Update(tmphistory)
-                # print(tmphistory, '----')
-                window.FindElement('_history_').Update(disabled=False)  
+                tmphistory = window.FindElement('_history_').GetListValues()
+                tmphistory.append(tmpcmd)
                 window.FindElement('_history_').Update(tmphistory)
-                window.FindElement('_history_').Update(disabled=True)
+                window.FindElement('_command_').Update("")
             except:
-                print(tmpcmd)
                 pass
     if event is "执行文件":
-        # filedialog.askopenfilename(initialdir ='.')
-        file_name = filedialog.askopenfilename(filetypes=(("小海龟角本文件","*.psy"),), initialdir ='.') # show the 'get file' dialog box
+        file_name = filedialog.askopenfilename(filetypes=(("小海龟脚本文件","*.psy"),), initialdir ='.') # show the 'get file' dialog box
         print(len(file_name), type(file_name))
         strcmd = "from turtle import *\n"
         if len(file_name)==0:
@@ -95,19 +84,6 @@ while True:
         else:
             with open(file_name) as f:
                 strcmd += f.read()
-            # print(strcmd)
             exec(strcmd, {}, {'t':t})
-        # print(t, type(t))
-        # exec("print(t, type(t),'==')", {}, {"t":t})
 
-        # print(values["command"], type(values))
-        
-    # elif event is 'Red':
-    #     window.FindElement('canvas').TKCanvas.itemconfig(cir, fill = "Red")
-
-
-# event, values = window.Read()    
 window.Close()
-
-# text_input = values[0]    
-# print(values)
